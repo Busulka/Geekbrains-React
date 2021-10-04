d9964126e554446af7b4600d062d7b686ec426d0
Busulka <83404095+Busulka@users.noreply.github.com>
2021-10-04 12:22:10 +0100

    done

--- homework/src/App.js
+++ homework/src/App.js
@@ -1,12 +1,10 @@
 import React, {useEffect, useState} from "react";
 import {MessageList} from "./components/MessageList";
-
-
-
-
+import {ChatList} from "./components/ChatList/ChatList";
 import TextField from '@material-ui/core/TextField';
 import {Button} from "@material-ui/core";
 import './App.css';
+
 
 
 function App (){
@@ -51,13 +49,34 @@ function App (){
             }
         },[messages]
     )
+
+    const chatArray = [
+        {
+            chatName: 'Cuisine',
+            id: '1',
+            text:'Lorem ipsum'
+        },
+        {
+            chatName: 'Entertainment',
+            id: '2',
+            text:'Lorem ipsum'
+        },
+        {
+            chatName: 'Travel',
+            id: '3',
+            text:'Lorem ipsum'
+        },
+
+
+    ]
     return (
         <div className = "chat">
+            <ChatList chatArray = {chatArray}/>
             <MessageList  messages={messages}/>
 
             <form className="inputBox" noValidate autoComplete="off">
 
-                <TextField className="input" id="filled-basic" label="Write your message here" variant="outlined" color="secondary" value={inputMessage} onChange={onChange} type="text"/>
+                <TextField className="input" id="filled-basic" label="Write your message here" variant="outlined" color="secondary" autoFocus value={inputMessage} onChange={onChange} type="text"/>
                 <Button type="button" variant="contained" color="primary" className= "sendMsg" onClick={() =>{
                     addMessage(createMessage("User", inputMessage, Date.now()
                     ));
