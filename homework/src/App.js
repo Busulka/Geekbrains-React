69d63afb50a9665d0ba922814b374797a7582f07
Busulka <83404095+Busulka@users.noreply.github.com>
2021-10-05 02:28:49 +0100

    Initial structure

--- homework/src/App.js
+++ homework/src/App.js
@@ -1,92 +1,23 @@
-import React, {useEffect, useState} from "react";
-import {MessageList} from "./components/MessageList";
-import {ChatList} from "./components/ChatList/ChatList";
-import TextField from '@material-ui/core/TextField';
-import {Button} from "@material-ui/core";
+import React from "react";
+import {Header} from "./components/Header";
+import {Layout} from "./components/Layout";
+import {Routes} from "./Routes/Routes";
+import {BrowserRouter} from "react-router-dom";
 import './App.css';
 
 
+function App() {
 
-function App (){
-    const createMessage = (author, inputMessage, key) => ({
-        author,
-        inputMessage,
-        key,
-    })
-    const [inputMessage, setInputMessage] = useState('');
-
-    const onChange = (event) => {
-        setInputMessage(event.target.value);
-    }
-
-    const [messages, setMessages] = useState([]);
-
-    const addMessage = (...items) => {
-        setMessages((messages)=>{
-            return [
-                ...messages,
-                ...items,
-            ]
-        })
-    }
-    useEffect(() =>{
-
-            if (messages.length !== 0) {
-                {
-                    if (messages[messages.length - 1].author === "User") {
-
-
-                        setTimeout(() => {
-
-                            addMessage(createMessage("Bot", inputMessage+" to you too", Date.now()+1))
-
-                        }, 2000);
-
-
-
-                    }
-                }
-            }
-        },[messages]
-    )
-
-    const chatArray = [
-        {
-            chatName: 'Cuisine',
-            id: '1',
-            text:'Lorem ipsum'
-        },
-        {
-            chatName: 'Entertainment',
-            id: '2',
-            text:'Lorem ipsum'
-        },
-        {
-            chatName: 'Travel',
-            id: '3',
-            text:'Lorem ipsum'
-        },
-
-
-    ]
     return (
-        <div className="allChats">
-            <ChatList chatArray = {chatArray}/>
-        <div className = "chat">
-
-            <MessageList  messages={messages}/>
-
-            <form className="inputBox" noValidate autoComplete="off">
-
-                <TextField className="input" id="filled-basic" label="Write your message here" variant="outlined" color="secondary" autoFocus value={inputMessage} onChange={onChange} type="text"/>
-                <Button type="button" variant="contained" color="primary" className= "sendMsg" onClick={() =>{
-                    addMessage(createMessage("User", inputMessage, Date.now()
-                    ));
-                }}>Send</Button>
-
-            </form>
-        </div>
-        </div>
-    )
+        <BrowserRouter>
+            <div className="App">
+                <Header/>
+                <Layout>
+                    <Routes />
+                </Layout>
+            </div>
+        </BrowserRouter>
+    );
 }
+
 export default App;
