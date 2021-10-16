62e9ae9b1b5b8da28d9e6ab7cd92a8484efa3118
Busulka <83404095+Busulka@users.noreply.github.com>
2021-10-16 11:43:57 +0300

    redid everything according to the lesson

--- homework/src/Routes/Chats/index.js
+++ homework/src/Routes/Chats/index.js
@@ -1,13 +1,26 @@
 import React from "react";
 import {ChatList} from "../../components/ChatList/ChatList";
-
 import {ChatArray} from "../../components/ChatArray/ChatArray";
+import {chatsAddChat} from "../../Store/chats/actions";
 
 
-export const Chats = () => {
+export const Chats = (props) => {
 
     return (
-        <ChatList chatArray = {ChatArray}/>
+        <div>
+            <ChatList chatArray = {ChatArray}/>
+            <button onClick={() =>{
+                chatsAddChat({
+                    id: Date.now(),
+                    chatName: 'Title',
+                    text: 'text',
+                })
+            }}>
+                Add Chat
+            </button>
+        </div>
+
+
     );
 };
 
