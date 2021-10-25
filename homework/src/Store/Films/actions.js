e3d17fc0a06f49d65e9c323c1c55a027a1e75ec0
Busulka <83404095+Busulka@users.noreply.github.com>
2021-10-25 23:50:32 +0300

    homework 9 done - chats and messages on Realtime Database

--- homework/src/Store/Films/actions.js
+++ homework/src/Store/Films/actions.js
@@ -5,22 +5,22 @@ export const REQUEST_SUCCESS = "MOVIES::REQUEST_SUCCESS";
 export const REQUEST_ERROR = "MOVIES::REQUEST_ERROR";
 export const MAP_MOVIE_DATA = "MOVIES::MAP_DATA";
 
-export const getMoviesPending = () => ({
+const getMoviesPending = () => ({
     type: REQUEST_PENDING,
 });
 
-export const getMoviesSuccess = (moviesData) => ({
+const getMoviesSuccess = (moviesData) => ({
     type: REQUEST_SUCCESS,
     payload: moviesData,
 });
 
-export const getMoviesError = (error) => ({
+const getMoviesError = (error) => ({
     type: REQUEST_ERROR,
     payload: error,
 });
 
 
-export const mapMoviesData = (moviesData) => ({
+const mapMoviesData = (moviesData) => ({
     type: MAP_MOVIE_DATA,
     payload: moviesData,
 })
